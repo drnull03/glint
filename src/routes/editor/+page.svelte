f333c1d9595df707a585485e41b449fa58e863d0
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2025-02-07 01:19:14 +0300

    Trying out the text-align extension

--- src/routes/editor/+page.svelte
+++ src/routes/editor/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-    import { Tipex, Utility } from "@friendofsvelte/tipex";
+    import { Tipex, Utility, defaultExtensions } from "@friendofsvelte/tipex";
+    import TextAlign from "@tiptap/extension-text-align";
     import "@friendofsvelte/tipex/styles/Tipex.css";
     import "@friendofsvelte/tipex/styles/ProseMirror.css";
     import "@friendofsvelte/tipex/styles/Controls.css";
@@ -11,30 +12,49 @@
 
     const save = () => {
         console.log(editor.getJSON());
+        console.log(editor.isEmpty);
+        if(editor.isEmpty) {
+            alert("Editor is empty");
+            return;
+        }
         display.commands.setContent(editor.getJSON());
     }
 
     let display;
     onMount(() => {
         display.setEditable(false);
+        console.log(display.can().toggleBold());
     })
 
     let displayBody = "<p>Your saved text will appear here</p>";
+
+    let editorExtensions = [
+        ...defaultExtensions,
+        TextAlign.configure({
+            types: ['heading', 'paragraph'],
+            alignments: ['left', 'right'],
+        })
+    ];
 </script>
 
 <main>
-    <Tipex bind:tipex={editor} class="tipex" controls floating>
+    <Tipex extensions={editorExtensions} bind:tipex={editor} class="editor" controls floating>
         {#snippet utilities(tipex)}
             <Utility {tipex}/>
         {/snippet}
     </Tipex>
     <button on:click={save}>Save</button>
-    <Tipex bind:tipex={display} body={displayBody} class="tipex" floating>
+    <Tipex bind:tipex={display} class="editor" body={displayBody}>
     </Tipex>
 </main>
 
 <style>
     main {
         font-family: sans-serif;
+    }
+
+    :global(.editor) {
+        max-width: 600px;
+        margin-inline: auto;
     }
 </style>
