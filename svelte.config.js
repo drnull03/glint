0162b58cc02dfc45ebc1f6e9f2856b0e3bd640a4
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2025-04-14 22:21:37 +0300

    Added Tauri for desktop apps

--- svelte.config.js
+++ svelte.config.js
@@ -2,26 +2,29 @@ import adapterStatic from '@sveltejs/adapter-static';
 import adapterAuto from '@sveltejs/adapter-auto';
 
 const isMobile = process.env.TARGET === 'mobile';
+const isDesktop = process.env.TARGET === 'desktop';
+
+let routes;
+if(isMobile) {
+  routes = 'src/mobile-routes';
+} else if(isDesktop) {
+  routes = 'src/desktop-routes'
+} else {
+  routes = 'src/routes'
+}
 
 const config = {
   kit: {
-    adapter: isMobile
+    adapter: (isMobile || isDesktop)
       ? adapterStatic({
-          pages: 'build-mobile',
-          assets: 'build-mobile',
+          pages: isDesktop ? 'build-desktop' : 'build-mobile',
+          assets: isDesktop ? 'build-desktop' : 'build-mobile',
           fallback: null,
-          strict: false // ignore dynamic routes (since they won't be present in mobile)
+          strict: false
         })
       : adapterAuto(),
-    // prerender: isMobile
-    //   ? {
-    //       crawl: false,       // Prevent automatic crawling
-    //       entries: ['/mobile'] // Only prerender the /mobile route
-    //     }
-    //   : {},
     files: {
-      // Use only the mobile routes folder when building for mobile
-      routes: isMobile ? 'src/mobile-routes' : 'src/routes'
+      routes
     }
   }
 };
